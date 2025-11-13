44e225a4dbd3c289ab1d663ee7212c856f72ba67
Keyvan Abbasmajidi <keyvan.a.majidi@gmail.com>
2025-11-13 08:36:12 +0100

    final JS update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-
-
 var randomNumber1 = Math.floor ( Math.random() * 6) + 1;
 var randomNumber2 = Math.floor ( Math.random() * 6) +  1;
 
 var imageSource1 = "dice" + randomNumber1 + ".png";
 var imageSource2 = "dice" + randomNumber2 + ".png";
 
-var image1 = document.querySelectorAll[0]("img");
-var image2 = document.querySelectorAll[1]("img");
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
 
 image1.setAttribute("src", imageSource1);
 image2.setAttribute("src", imageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "Darw";
+}
